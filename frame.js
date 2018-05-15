031ed04d4f086710b02eb304f891a18785ea6449
Ekaterina Simonova <simonova.katie@gmail.com>
2018-05-15 13:58:57 +0500

    add frame.js

---
+++ frame.js
@@ -0,0 +1,26 @@
+function contentSize() {
+    var body = document.compatMode &&
+    document.compatMode == 'CSS1Compat'
+        ? document.documentElement
+        : document.body;
+
+    var height = Math.max(body.scrollHeight, body.offsetHeight);
+    return height;
+};
+
+var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+var eventer = window[eventMethod];
+var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+var content = {};
+
+eventer(messageEvent, function (e) {
+    content.id = e.data;
+    console.log(content.id);
+}, false);
+
+content.height = contentSize();
+
+setTimeout(function () {
+    console.log('content', content);
+    window.parent.postMessage(content, '*');
+}, 0);
